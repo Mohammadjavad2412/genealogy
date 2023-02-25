cd663890cfa8919c2043adf8c6658f7fdd66f485
Mohammad Eivazi <m.eivazi@yas.group>
2023-02-25 15:14:22 +0330

    debug

--- contracts/Genealogy.sol
+++ contracts/Genealogy.sol
@@ -3,7 +3,6 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/utils/Strings.sol";
 import "github.com/Arachnid/solidity-stringutils/blob/master/src/strings.sol";
 
-
 contract Genealogy {
     using strings for *;
     struct Partner {
@@ -17,17 +16,17 @@ contract Genealogy {
         uint256 last_update;
         bool isValue;
     }
-    mapping(uint256 => Partner) partnersByPositionId;
+    mapping(uint256 => Partner) public partnersByPositionId;
     mapping(address => Partner) partnersByWalletAddress;
     mapping(string => Partner) partnersByEbrCode;
     mapping(address => string[]) invitationLinksByreferallWalletAdress;
     mapping(address => string[]) invitationEbrPartnersByreferallWalletAddress;
-    string[] refferalCodes;
+    string[] public refferalCodes;
     uint256 left_child;
     uint256 sub_child_numbers;
     uint256 sub_child;
     uint256 position_id_from_ebr_code;
-    mapping(string=>Partner) Childs;
+    mapping(string => Partner) Childs;
     uint256 partnerCount;
     uint256[] position_ids;
     uint256 upline_position_id;
@@ -79,97 +78,152 @@ contract Genealogy {
             return false;
         }
     }
-    
-    function IsValidEbrCode(string memory _ebr_code) internal view returns(bool){
-        if (partnersByEbrCode[_ebr_code].isValue){
+
+    function IsValidEbrCode(string memory _ebr_code)
+        internal
+        view
+        returns (bool)
+    {
+        if (partnersByEbrCode[_ebr_code].isValue) {
             return true;
-        }
-        else{
+        } else {
             return false;
         }
     }
 
-    function createReffralString(string memory _invitation_ebr_code, string memory _refferal_ebr_code, uint _position_id) internal pure returns (string memory) {
-        return string(abi.encodePacked(_invitation_ebr_code,'-',_refferal_ebr_code,'-',Strings.toString(_position_id)));
+    function createReffralString(
+        string memory _invitation_ebr_code,
+        string memory _refferal_ebr_code,
+        uint256 _position_id
+    ) internal pure returns (string memory) {
+        return
+            string(
+                abi.encodePacked(
+                    _invitation_ebr_code,
+                    "-",
+                    _refferal_ebr_code,
+                    "-",
+                    Strings.toString(_position_id),
+                    "-"
+                )
+            );
     }
 
-    function addRefferalCode(string memory _refferalCode)public{
+    function addRefferalCode(string memory _refferalCode) public {
         refferalCodes.push(_refferalCode);
     }
 
-    function addInvitaionLinksByWalletAddress(string memory _refferalLink)internal{
+    function addInvitaionLinksByWalletAddress(string memory _refferalLink)
+        internal
+    {
         invitationLinksByreferallWalletAdress[msg.sender].push(_refferalLink);
     }
 
-    function addInvitationEbrCodesByWalletAddress(string memory _invitation_code)internal{
-        invitationEbrPartnersByreferallWalletAddress[msg.sender].push(_invitation_code);
+    function addInvitationEbrCodesByWalletAddress(
+        string memory _invitation_code
+    ) internal {
+        invitationEbrPartnersByreferallWalletAddress[msg.sender].push(
+            _invitation_code
+        );
     }
 
-    function isValidWalletAddress() public view returns(bool){
+    function isValidWalletAddress() public view returns (bool) {
         address sender = msg.sender;
-        if (partnersByWalletAddress[sender].isValue==true){
+        if (partnersByWalletAddress[sender].isValue == true) {
             return true;
-        }
-        else{
+        } else {
             return false;
         }
     }
 
-    function myInvitationLinks()public view returns(string[] memory){
+    function myInvitationLinks() public view returns (string[] memory) {
         return invitationLinksByreferallWalletAdress[msg.sender];
     }
 
-    function myInvitationEbrsPartner()public view returns(string[] memory){
+    function myInvitationEbrsPartner() public view returns (string[] memory) {
         return invitationEbrPartnersByreferallWalletAddress[msg.sender];
     }
 
-    function isValidInvitationLink(string memory _invitation_link) public view returns(bool){
-        for (uint i=0;i<refferalCodes.length;i++){
-            if (keccak256(abi.encodePacked(refferalCodes[i]))==keccak256(abi.encodePacked(_invitation_link))){
+    function isValidInvitationLink(string memory _invitation_link)
+        public
+        view
+        returns (bool)
+    {
+        for (uint256 i = 0; i < refferalCodes.length; i++) {
+            if (
+                keccak256(abi.encodePacked(refferalCodes[i])) ==
+                keccak256(abi.encodePacked(_invitation_link))
+            ) {
                 return true;
             }
         }
         return false;
     }
 
-    function createRefferalCode(string memory _invitation_ebr_code,uint _position_id) public returns(string memory){
-        require(IsValidEbrCode(_invitation_ebr_code)==false,"duplicate ebr code!");
+    function createRefferalCode(
+        string memory _invitation_ebr_code,
+        uint256 _position_id
+    ) public returns (string memory) {
+        require(
+            IsValidEbrCode(_invitation_ebr_code) == false,
+            "duplicate ebr code!"
+        );
         uint256 _upline = calcUplineFromPositionId(_position_id);
-        require(isValidPositionId(_upline),"invalid position id.Are you sure that has upline?");
-        require(isValidPositionId(_position_id)==false,"this position is already filled!");
+        require(
+            isValidPositionId(_upline),
+            "invalid position id.Are you sure that has upline?"
+        );
+        require(
+            isValidPositionId(_position_id) == false,
+            "this position is already filled!"
+        );
         Partner memory refferal_partner = partnersByWalletAddress[msg.sender];
-        string memory refferalCode = createReffralString(_invitation_ebr_code,refferal_partner.ebr_code,_position_id);
+        string memory refferalCode = createReffralString(
+            _invitation_ebr_code,
+            refferal_partner.ebr_code,
+            _position_id
+        );
         addInvitaionLinksByWalletAddress(refferalCode);
         addInvitationEbrCodesByWalletAddress(_invitation_ebr_code);
         refferalCodes.push(refferalCode);
         return refferalCode;
     }
 
-    function addPartnerFromLink(string memory _invitation_link) public returns(string memory result){
-        string memory direction = 'l';
+    function addPartnerFromLink(string memory _invitation_link)
+        public
+        payable
+        returns (string memory result)
+    {
+        string memory direction = "l";
         string memory invitation_link = _invitation_link;
-        require(isValidWalletAddress()==false,"you had position.");
-        require(isValidInvitationLink(invitation_link),"invalid invitation link");
-        strings.slice memory s = invitation_link.toSlice();                
-        strings.slice memory delim = "-".toSlice();                            
-        string[] memory parts = new string[](s.count(delim));                  
-        for (uint i = 0; i < parts.length; i++) {                              
-           parts[i] = s.split(delim).toString();                               
-        }                                        
-        require(parts.length==3,"invalid ebr code");
+        require(isValidWalletAddress() == false, "you had position.");
+        require(
+            isValidInvitationLink(invitation_link),
+            "invalid invitation link"
+        );
+        strings.slice memory s = invitation_link.toSlice();
+        strings.slice memory delim = "-".toSlice();
+        string[] memory parts = new string[](s.count(delim));
+        for (uint256 i = 0; i < parts.length; i++) {
+            parts[i] = s.split(delim).toString();
+        }
+        // require(parts.length==3,"invalid ebr code");
         string memory invited_ebr_code = parts[0];
         string memory refferal_ebr_code = parts[1];
         uint256 position_id = stringToUint(parts[2]);
-        require(isValidPositionId(position_id)==false,"this position is already filled");
+        require(
+            isValidPositionId(position_id) == false,
+            "this position is already filled"
+        );
         uint256 upline_position_id = calcUplineFromPositionId(position_id);
-        string memory  upline_ebr_code = partnersByPositionId[upline_position_id].ebr_code;
-        if (position_id%2==0){
-            string memory direction = 'r';
+        string memory upline_ebr_code = partnersByPositionId[upline_position_id]
+            .ebr_code;
+        if (position_id % 2 == 0) {
+            string memory direction = "r";
+        } else {
+            string memory direction = "l";
         }
-        else{
-            string memory direction = 'l';
-        }
-        this.addPartner(invited_ebr_code,direction,upline_ebr_code,0);
+        this.addPartner(invited_ebr_code, direction, upline_ebr_code, 0);
         return "successfully add partner";
     }
 
@@ -203,15 +257,14 @@ contract Genealogy {
             Partner storage partner = partnersByEbrCode[_ebr_code];
             position_id_from_ebr_code = partner.position_id;
             return Strings.toString(position_id_from_ebr_code);
-        }
-        else{
+        } else {
             return "none";
         }
     }
 
-    function stringToUint(string memory s) internal pure returns (uint) {
+    function stringToUint(string memory s) internal pure returns (uint256) {
         bytes memory b = bytes(s);
-        uint result = 0;
+        uint256 result = 0;
         for (uint256 i = 0; i < b.length; i++) {
             uint256 c = uint256(uint8(b[i]));
             if (c >= 48 && c <= 57) {
@@ -233,9 +286,10 @@ contract Genealogy {
         ) {
             upline_position_id = 0;
             new_position_id = 0;
-        }
-        else {
-            uint256 upline_position_id = stringToUint(getPositionIdFromEbrCode(_upline_ebr_code));
+        } else {
+            uint256 upline_position_id = stringToUint(
+                getPositionIdFromEbrCode(_upline_ebr_code)
+            );
             uint256 new_position_id = generatePositionId(
                 upline_position_id,
                 _direction
@@ -256,7 +310,7 @@ contract Genealogy {
         partnersByEbrCode[_ebr_code] = partner;
         position_ids.push(new_position_id); // storing all position ids
         partnersByPositionId[new_position_id] = partner;
-        partnersByWalletAddress[msg.sender]=partner;
+        partnersByWalletAddress[msg.sender] = partner;
         partnerCount++;
     }
 
@@ -273,7 +327,7 @@ contract Genealogy {
         view
         returns (Partner memory)
     {
-        require(isValidPositionId(_position_id),"invalid position id");
+        require(isValidPositionId(_position_id), "invalid position id");
         return partnersByPositionId[_position_id];
     }
 
@@ -304,24 +358,35 @@ contract Genealogy {
         return (id, wallet_address, ebr_code, direction, balance);
     }
 
-    function calcNextChilds(uint256 _position_id) public pure returns(uint256,uint256){
+    function calcNextChilds(uint256 _position_id)
+        public
+        pure
+        returns (uint256, uint256)
+    {
         uint256 left_child_position = _position_id * 2 + 1;
         uint256 right_child_position = _position_id * 2 + 2;
-        return (left_child_position,right_child_position);
+        return (left_child_position, right_child_position);
     }
 
-    function calcUplineFromPositionId(uint256 _position_id) public pure returns (uint256){
-        if (_position_id%2==0){
-            return (_position_id-2)/2;
-        }
-        else{
-            return (_position_id-1)/2;
+    function calcUplineFromPositionId(uint256 _position_id)
+        public
+        pure
+        returns (uint256)
+    {
+        if (_position_id % 2 == 0) {
+            return (_position_id - 2) / 2;
+        } else {
+            return (_position_id - 1) / 2;
         }
     }
 
-    function getBalanceByPositionId(uint256 _position_id) public view returns (uint256){
+    function getBalanceByPositionId(uint256 _position_id)
+        public
+        view
+        returns (uint256)
+    {
         Partner memory partner = partnersByPositionId[_position_id];
-        require(isValidPositionId(_position_id),"invalid position id");
+        require(isValidPositionId(_position_id), "invalid position id");
         return partner.balance;
-        }
+    }
 }
